4dc33ae36c050c1a0a67b37353691492d73b24d4
pluxurypinkphloydrockstar <theDianaB15@gmail.com>
2023-07-06 16:36:30 +0300

    Add files via upload

---
+++ EulerMethod/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace EulerMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Fuchsia;
+            this.button2.Location = new System.Drawing.Point(915, 153);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(303, 97);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Метод Эйлера";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Fuchsia;
+            this.button1.Location = new System.Drawing.Point(915, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 97);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Fuchsia;
+            this.textBox1.Location = new System.Drawing.Point(721, 69);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 37);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(745, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Введите N:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(685, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Максимальное значение невязки:\r\n";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(748, 171);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Fuchsia;
+            this.button3.Location = new System.Drawing.Point(915, 272);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(303, 97);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Построить график точного решения";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1240, 687);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ EulerMethod/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+
+namespace EulerMethod
+{
+    public partial class Form1 : Form
+    {
+        public int n;
+        public string varPoint = "0";
+
+        ZedGraphControl zedGrapgControl1 = new ZedGraphControl();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            zedGrapgControl1.Location = new Point(10, 30);
+            zedGrapgControl1.Name = "text";
+            zedGrapgControl1.Size = new Size(500, 500);
+            Controls.Add(zedGrapgControl1);
+            GraphPane my_Pane = zedGrapgControl1.GraphPane;
+            my_Pane.Title.Text = "Результат:";
+            my_Pane.XAxis.Title.Text = "X";
+            my_Pane.YAxis.Title.Text = "Y";
+        }
+        private void GetSize()
+        {
+            zedGrapgControl1.Location = new Point(10, 10);
+            zedGrapgControl1.Size = new Size(ClientRectangle.Width - 20, ClientRectangle.Height - 20);
+        }
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            GetSize();
+        }
+        static double f1(double x, double y)//Исходное дифференциальное уравнение
+        {
+            return ((2 * x * Math.Pow(y, -2) * Math.Pow(Math.E, -2 * x * x)) - (2 * x * y)) / 3;
+        }
+        static double f2(double x)//Точное решение задачи Коши 
+        {
+            double pw = ((0 * Math.Pow(Math.E, Math.Pow(x, 2))) - 1);
+            return ((-1) / (Math.Pow(Math.E, ((2 * Math.Pow(x, 2)) / 3))));
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Clear(zedGrapgControl1);
+            GriddenOn(zedGrapgControl1.GraphPane);
+        }
+        private void Clear(ZedGraphControl Zed_GraphControl)
+        {
+            //GraphPane pane = Zed_GraphControl.GraphPane;
+            zedGrapgControl1.GraphPane.CurveList.Clear();
+            zedGrapgControl1.GraphPane.GraphObjList.Clear();
+
+            zedGrapgControl1.GraphPane.XAxis.Type = AxisType.Linear;
+            zedGrapgControl1.GraphPane.XAxis.Scale.TextLabels = null;
+            zedGrapgControl1.GraphPane.XAxis.MajorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.YAxis.MajorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.YAxis.MinorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.XAxis.MinorGrid.IsVisible = false;
+            zedGrapgControl1.RestoreScale(zedGrapgControl1.GraphPane);
+
+            zedGrapgControl1.AxisChange();
+            zedGrapgControl1.Invalidate();
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                varPoint = "0";
+                MessageBox.Show("Введите N:");
+            }
+            else
+            {
+                varPoint = "1";
+                n = Convert.ToInt32(textBox1.Text);
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Eiler(ZedGraphControl Zed_GraphControl)//сам метод ломанных Эйлера
+        {
+            GraphPane my_Pane = Zed_GraphControl.GraphPane;
+            PointPairList list = new PointPairList();
+            try
+            {
+                double maxNev, curNev, h, x, y;
+                int i;
+                maxNev = 0; curNev = maxNev;
+                x = 0; y = -1;
+                h = (double)(1 - 0) / n;
+                for (i = 0; i < n + 1; i++)
+                {
+                    list.Add(x, y);
+                    curNev = Math.Abs((y + h * f1(x, y)) - f2(x));
+                    if (curNev > maxNev)
+                    {
+                        maxNev = curNev;
+
+                    }
+                    y = y + h * f1(x, y);
+                    x += h;
+                }
+                PointPairList listMIN = new PointPairList();
+                PointPairList listMAX = new PointPairList();
+
+                LineItem d1 = my_Pane.AddCurve("Результат метода Эйлера", list, Color.Blue, SymbolType.None);
+                textBox2.Text = maxNev.ToString();
+                zedGrapgControl1.AxisChange();
+                zedGrapgControl1.Invalidate();
+            }
+            catch
+            {
+                MessageBox.Show("Некорректный ввод данных.");
+            }
+        }
+        private void GriddenOn(GraphPane my_Pane)
+        {
+            //GraphPane my_Pane = Zed_GraphControl.GraphPane;
+            my_Pane.XAxis.MajorGrid.IsVisible = true;
+            my_Pane.YAxis.MajorGrid.IsVisible = true;
+            my_Pane.YAxis.MinorGrid.IsVisible = true;
+            my_Pane.XAxis.MinorGrid.IsVisible = true;
+        }
+        private void BTN_EM(object sender, EventArgs e)
+        {
+            GriddenOn(zedGrapgControl1.GraphPane);
+            Eiler(zedGrapgControl1);
+        }
+        private void Rez(ZedGraphControl Zed_GraphControl)//Построение графика точного решения
+        {
+            double a, b, h1; int i;
+            a = 0; b = 1;
+            h1 = (double)(b - a) / 100D;
+            double[] coorX = new double[100 + 1];
+            double[] coorY = new double[100 + 1];
+            GraphPane my_Pane = Zed_GraphControl.GraphPane;
+            PointPairList list2 = new PointPairList();
+            for (i = 0; i < 100 + 1; i++)
+            {
+                coorX[i] = a + i * h1;
+                coorY[i] = f2(coorX[i]);
+                list2.Add(coorX[i], coorY[i]);
+            }
+            LineItem myCircle = my_Pane.AddCurve("Точное решение", list2, Color.Red, SymbolType.None);
+            zedGrapgControl1.AxisChange();
+            zedGrapgControl1.Invalidate();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GriddenOn(zedGrapgControl1.GraphPane);
+            Eiler(zedGrapgControl1);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GriddenOn(zedGrapgControl1.GraphPane);
+            Rez(zedGrapgControl1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
